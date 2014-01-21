ed8b17b74670c735378d811d4c8b11b22afb4fe1
mkayokom <mkayokom@gmail.com>
2014-01-21 19:01:00 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :microposts, dependent: :destroy
 	has_secure_password 
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
@@ -16,6 +17,11 @@ class User < ActiveRecord::Base
 
   def User.encrypt(token)
   	Digest::SHA1.hexdigest(token.to_s) #トークンを暗号化する関数を定義			
+  end
+
+  def feed
+    #　このコードは準備段階。11章で。
+    Micropost.where("user_id = ?", id)
   end
 
   private
